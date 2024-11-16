73839a993aafc5e8f712f5697019eff529dd3ad0
Naoya Iijima <meteor5963@gmail.com>
2024-11-17 01:57:03 +0900

    開発環境用にセットアップ

--- frontend/app/api/posts/layout.tsx
+++
@@ -1,8 +0,0 @@
-// app/api/posts/layout.tsx
-export default function PostsLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <>{children}</>;
-}
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // webpackの設定を変更
-  webpack: (config, { dev }) => {
-    if (dev) {
-      // 開発環境のみhttpsを無効化
-      config.devServer = {
-        ...(config.devServer || {}),
-        https: false, // httpsを無効にする
-      };
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
